b911ad0706b5fba4fda2e9cde5e7d5bc2cafe165
Devesh sahu <deveshsahu567@gmail.com>
2026-07-09 13:05:09 +0530

    Fix forgot password OTP auth routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const router = express.Router();
-
 const {
   registerUser,
   loginUser,
@@ -8,13 +6,14 @@ const {
   forgotPassword,
   resetPassword,
 } = require("../controllers/authController");
-
 const { protect } = require("../middleware/authMiddleware");
+
+const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.get("/me", protect, getMe);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
-router.get("/me", protect, getMe);
 
 module.exports = router;
